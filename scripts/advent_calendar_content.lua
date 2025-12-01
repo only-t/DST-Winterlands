464ab5bc169bca61fcdefc8a6073058d175c645d
Luкaട <onlyt.coding@gmail.com>
2025-12-01 14:11:09 +0100

    Tweaked advent calendar.

--- scripts/advent_calendar_content.lua
+++ scripts/advent_calendar_content.lua
@@ -1,71 +1,8 @@
-local date = os.date("%x")
-ADVENT_DAY = 0
+function HasPassedCalendarDay(day)
+    local offset = -8 * 3600
+    local date = os.date("!*t", os.time() + offset)
 
--- December 1st content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/01/25" then
-    return
+    return date.month ~= 12 or date.day >= day
 end
 
--- December 2nd content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/02/25" then
-    return
-end
-
--- December 3rd content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/03/25" then
-    return
-end
-
--- December 4th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/04/25" then
-    return
-end
-
--- December 5th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/05/25" then
-    return
-end
-
--- December 6th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/06/25" then
-    return
-end
-
--- December 7th content
-
-if date == "12/07/25" then
-    return
-end
-
--- December 8th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/08/25" then
-    return
-end
-
--- December 9th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/09/25" then
-    return
-end
-
--- December 10th content
-
-ADVENT_DAY = ADVENT_DAY + 1
-if date == "12/10/25" then
-    return
-end
+FINAL_ADVENT_DAY = 10 -- Will increase it to 24 once all updates are finished
